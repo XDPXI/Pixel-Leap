6c182c3c8d72b14d0936da3f0a9a8e1aed431139
Ztrolix <xdpxigaming@gmail.com>
2025-01-04 21:50:52 +1100

    Fixed Performance Issues

--- game/src/main/java/Main.java
+++ game/src/main/java/Main.java
@@ -17,11 +17,9 @@ public class Main {
 
         for (int i = 0; i < args.length; i++) {
             if ("--map".equals(args[i]) || args[i].startsWith("--map=")) {
-                // If it's the equals format (e.g., --map=2)
                 if (args[i].contains("=")) {
-                    mapNumber = args[i].split("=")[1];  // Split at '=' and extract map number
+                    mapNumber = args[i].split("=")[1];
                 }
-                // If it's the space format (e.g., --map 2)
                 else if (i + 1 < args.length) {
                     mapNumber = args[i + 1];
                 }
@@ -42,13 +40,10 @@ public class Main {
     }
 
     private static void getWidthAndHeight() {
-        // Get the local graphics environment
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 
-        // Get the array of graphics devices (monitors)
         GraphicsDevice[] devices = ge.getScreenDevices();
 
-        // Iterate through each monitor and get its dimensions
         for (int i = 0; i < devices.length; i++) {
             DisplayMode displayMode = devices[i].getDisplayMode();
             System.out.println("Monitor " + (i + 1) + ":");
@@ -65,22 +60,21 @@ public class Main {
         try {
             UIManager.setLookAndFeel(new FlatDarkLaf());
         } catch (UnsupportedLookAndFeelException e) {
-            showError("Failed to set FlatDarkLaf look and feel", e);
+            showError(e);
         }
     }
 
-    private static void showError(String message, Exception e) {
-        JOptionPane.showMessageDialog(null, message);
+    private static void showError(Exception e) {
+        JOptionPane.showMessageDialog(null, "Failed to set FlatDarkLaf look and feel");
         if (e != null) {
             Logger logger = Logger.getLogger(Main.class.getName());
-            logger.log(Level.SEVERE, message, e);
+            logger.log(Level.SEVERE, "Failed to set FlatDarkLaf look and feel", e);
         }
     }
 
     public static void restartApplication(int mapID) throws IOException {
         String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
 
-        // Get the current file path
         File currentFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         System.out.println("Current file: " + currentFile);
 
@@ -88,28 +82,24 @@ public class Main {
             throw new IOException("Unable to locate the running application file.");
         }
 
-        // Prepare the command
         ArrayList<String> command = new ArrayList<>();
         command.add(javaBin);
 
-        // Handle .jar and non-jar scenarios
         if (currentFile.getName().endsWith(".jar")) {
             command.add("-jar");
             command.add(currentFile.getPath());
-            command.add("--map=" + mapID);  // Pass mapID as an argument
+            command.add("--map=" + mapID);
         } else {
             command.add("-cp");
             command.add(currentFile.getPath());
             command.add(Main.class.getName());
-            command.add("--map=" + mapID);  // Pass mapID as an argument
+            command.add("--map=" + mapID);
         }
 
-        // Start the process
         ProcessBuilder processBuilder = new ProcessBuilder(command.toArray(new String[0]))
-                .inheritIO();  // Inherit IO to see output in the current terminal
+                .inheritIO();
         processBuilder.start();
 
-        // Exit the current application
         System.exit(0);
     }
 }
--- game/src/main/java/Maps.java
+++ game/src/main/java/Maps.java
@@ -2,7 +2,7 @@ public class Maps {
     // Map 1
     public static final Platform[] map1 = {
             // Dirt - Brown
-            new Platform(-1_073_741_823f, -2_147_483_647f, 2_147_483_647f, 2_147_483_647f, "#7B3F00"),
+            new Platform(-1_073_741_823f, -2000f, 2_147_483_647f, 2000f, "#7B3F00"),
             // Grass - Green
             new Platform(-1_073_741_823f, -100f, 2_147_483_647f, 100f, "#228B22"),
 
@@ -35,18 +35,18 @@ public class Maps {
             new Platform(2850f, 1700f, 100f, 20f, "#0FFF50"),
 
             // Void - Black
-            new Platform(-2_147_485_000f, -1_073_741_823f, 2_147_483_647f, 2_147_483_647f, "#000000")
+            new Platform(-5000f, -1_073_741_823f, 4000f, 2_147_483_647f, "#000000")
     };
 
     // Map 2
     public static final Platform[] map2 = {
             // Dirt - Brown
-            new Platform(-1_073_741_823f, -2_147_483_647f, 2_147_483_647f, 2_147_483_647f, "#7B3F00"),
+            new Platform(-1_073_741_823f, -2000f, 2_147_483_647f, 2000f, "#7B3F00"),
             // Grass - Green
             new Platform(-1_073_741_823f, -100f, 2_147_483_647f, 100f, "#228B22"),
 
             // Lava - Red
-            new Platform(300f, -2_147_483_647f, 2000f, 2_147_483_647f, "#FF2400"),
+            new Platform(300f, -2000f, 2000f, 2000f, "#FF2400"),
 
             // Normal - Gray
             new Platform(400f, 100f, 200f, 20f, "#4b6371"),
@@ -56,7 +56,7 @@ public class Maps {
             new Platform(2000f, 100f, 200f, 20f, "#4b6371"),
 
             // Lava - Red
-            new Platform(2700f, -2_147_483_647f, 500f, 2_147_483_647f, "#FF2400"),
+            new Platform(2700f, -2000f, 500f, 2000f, "#FF2400"),
 
             // Teleporter - Purple
             new Platform(2600f, 100f, 200f, 20f, "#7F00FF"),
@@ -66,18 +66,18 @@ public class Maps {
             new Platform(3300f, 0f, 100f, 20f, "#0FFF50"),
 
             // Void - Black
-            new Platform(-2_147_485_000f, -1_073_741_823f, 2_147_483_647f, 2_147_483_647f, "#000000")
+            new Platform(-5000, -1_073_741_823f, 4000, 2_147_483_647f, "#000000")
     };
 
     // Map 3
     public static final Platform[] map3 = {
             // Dirt - Brown
-            new Platform(-1_073_741_823f, -2_147_483_647f, 2_147_483_647f, 2_147_483_647f, "#7B3F00"),
+            new Platform(-1_073_741_823f, -2000f, 2_147_483_647f, 2000f, "#7B3F00"),
             // Grass - Green
             new Platform(-1_073_741_823f, -100f, 2_147_483_647f, 100f, "#228B22"),
 
             // Void - Black
-            new Platform(-2_147_485_000f, -1_073_741_823f, 2_147_483_647f, 2_147_483_647f, "#000000")
+            new Platform(-5000f, -1_073_741_823f, 4000f, 2_147_483_647f, "#000000")
     };
 
     public static Platform[] getMap(String mapID) {
